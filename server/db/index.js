e06567a515d6980ef694bd7acecaf6677f26a5d7
Vicki Wong <vicki.wong85@gmail.com>
2022-09-22 16:19:13 -0500

    trying client for 1000 users

--- server/db/index.js
+++ server/db/index.js
@@ -2,14 +2,14 @@ const { Pool, Client } = require('pg')
 const path = require('path');
 require('dotenv').config({path: path.resolve(__dirname, '../.env')});
 
-const client = new Pool({
+const client = new Client({
   host: process.env.HOST,
   user: process.env.USERNAME,
   port: process.env.DBPORT,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  idleTimeoutMillis: 0,
-  connectionTimeoutMillis: 0,
+  // idleTimeoutMillis: 0,
+  // connectionTimeoutMillis: 0,
 })
 client.connect();
 
