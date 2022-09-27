586ff281ab1b0a8558007778342c7a525ab58c36
Vicki Wong <vicki.wong85@gmail.com>
2022-09-27 11:41:30 -0500

    not sure which env dot env config

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,7 @@
 const { Pool, Client } = require('pg')
 const path = require('path');
-require('dotenv').config({ path: '../.env' });
+// require('dotenv').config({ path: '../.env' });
+require('dotenv').config({path: path.resolve(__dirname, '../.env')});
 
 const pool = new Pool({
   host: process.env.HOST,
